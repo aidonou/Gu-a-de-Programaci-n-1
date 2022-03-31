b0e39262234af0df275d118720cf8f6b56908697
aidonou <101400213+aidonou@users.noreply.github.com>
2022-03-31 15:54:26 -0300

    terminado ejercicio 8 y parte del ejercicio 9

--- Assets/scripts/ejercicio8.cs
+++ Assets/scripts/ejercicio8.cs
@@ -1,13 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Crear una variable entera que pueda ser modificada desde el Inspector. 
+//Suponiendo que el primer día de la semana es el Domingo, mostrar el nombre del día correspondiente al valor de la variable “dia”. 
+//Si el día no está en el rango  permitido debe emitir por pantalla el mensaje de error "El día ingresado no es válido". 
 public class ejercicio8 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int Dia;
     void Start()
     {
-        
+        switch(Dia)
+        {
+            case 1:
+                Debug.Log("Domingo");
+                break;
+            case 2:
+                Debug.Log("Lunes");
+                break;
+            case 3:
+                Debug.Log("Martes");
+                break;
+            case 4:
+                Debug.Log("Miercoles");
+                break;
+            case 5:
+                Debug.Log("Jueves");
+                break;
+            case 6:
+                Debug.Log("Viernes");
+                break;
+            case 7:
+                Debug.Log("Sabado");
+                break;
+            case 8:
+                Debug.Log("Domingo");
+                break;
+            default:
+                Debug.Log("El día ingresado no es válido");
+                break;
+        }
     }
 
     // Update is called once per frame
---
+++ Assets/scripts/ejercicio9.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Tres personas aportan diferente capital a una sociedad. Se desea saber qué porcentaje del  total aportó cada una (indicando nombre y porcentaje) y cuál es el monto del total aportado  por las tres.  
+//Ingresar por Inspector el ingreso del capital aportado por cada una de las personas y luego  mostrar lo pedido en el siguiente formato:  
+//Nombre: capital aportado: $ … , Porcentaje del capital: %.... , Monto total aportado: $ …. 
+
+public class ejercicio9 : MonoBehaviour
+{
+    public float capital1;
+    public float capital2;
+    public float capital3;
+    float capitaltotal;
+
+
+
+    void Start()
+    {
+        capitaltotal = capital1 + capital2 + capital3;
+        Debug.Log("El porcentaje de capital aportado de la primera persona es " + capital1 + " de " + capitaltotal);
+        Debug.Log("El porcentaje de capital aportado de la segunda persona es " + capital2 + " de " + capitaltotal);
+        Debug.Log("El porcentaje de capital aportado de la tercera persona es " + capital3 + " de " + capitaltotal);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
